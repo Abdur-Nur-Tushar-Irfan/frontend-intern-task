025b156f66cc29f06b34480662fed84fb9236173
Abdur Nur Tushar Irfan <tusharctg1202@gmail.com>
2022-12-01 18:45:55 +0600

    router-dom added

---
+++ src/Pages/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1>This is header</h1>
+            
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/Pages/Register/Register.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Register = () => {
+    return (
+        <div>
+            <h1>This is register form</h1>
+            
+        </div>
+    );
+};
+
+export default Register;
