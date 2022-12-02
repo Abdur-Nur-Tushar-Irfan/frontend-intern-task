5905351f9f539857b0bdf00ba9aed54a0f820cf5
Abdur Nur Tushar Irfan <tusharctg1202@gmail.com>
2022-12-02 08:51:54 +0600

    attendance table added

--- src/Pages/Attendence/Attendence.js
+++ src/Pages/Attendence/Attendence.js
@@ -1,11 +1,41 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 
 const Attendence = () => {
-    const {}=useQuery()
+    const { data: users = [] } = useQuery({
+        queryKey: ['users'],
+        queryFn: () => fetch('http://localhost:5000/users')
+            .then(res => res.json())
+    })
     return (
-        <div>
-            <h1>This is Attendence</h1>
-            
+        <div className="overflow-x-auto">
+            <h1 className='text-3xl font-bold'>Attendence Table</h1>
+            <table className="table w-full">
+           
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Number</th>
+                        <th>Email</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        users?.map((user,i)=> <tr>
+                            <th>{i+1}</th>
+                            <td>{user.firstName}</td>
+                            <td>{user.lastName}</td>
+                            <td>{user.number}</td>
+                            <td>{user.email}</td>
+                        </tr>)
+                    }
+                
+                  
+                   
+                </tbody>
+            </table>
         </div>
     );
 };
